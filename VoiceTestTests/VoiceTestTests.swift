d986c1db3f4768e60e89bd45ad016e1caa9d30d6
“Nj” <s1110832058@gmail.com>
2025-07-10 16:24:52 +0800

    Initial Commit

---
+++ VoiceTestTests/VoiceTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTestTests.swift
+//  VoiceTestTests
+//
+//  Created by imac-2626 on 2025/7/10.
+//
+
+import Testing
+@testable import VoiceTest
+
+struct VoiceTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
